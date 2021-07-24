a2897e030821a1059e5360abd69b5f6d0d010345
Dare Goodness <daregoodness@gmail.com>
2021-07-24 21:19:47 +0100

    tweetbox

--- src/Components/TweetBox/TweetBox.js
+++ src/Components/TweetBox/TweetBox.js
@@ -1,17 +1,37 @@
 import { Avatar, Button } from '@material-ui/core';
 import React, { useState } from 'react';
 import './TweetBox.css';
+import db from '../../firebase';
 
 const TweetBox = () => {
   const [tweetMessage, setTweetMessage] = useState('');
+  const [tweetImage, setTweetImage] = useState('');
+
+  const sendTweet = e => {
+    e.preventDefault();
+    db.collection('posts').add({
+      displayName: 'Dare Goodness',
+      username: '_DareGoodness',
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+    });
+  };
   return (
     <div className='tweetBox'>
       <form>
         <div className='tweetBoxInput'>
           <Avatar src=''></Avatar>
-          <input placeholder="What's happening?" type='text' />
+          <input
+            placeholder="What's happening?"
+            type='text'
+            value={tweetMessage}
+            onChange={e => setTweetMessage(e.target.value)}
+          />
         </div>
         <input
+          onChange={e => setTweetImage(e.target.value)}
+          value={tweetImage}
           placeholder='Optional: Enter image URL'
           type='text'
           className='tweetBoxImageInput'
