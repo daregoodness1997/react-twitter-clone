6a95d7fba7b4dcf9395b04c8c4d606c468f5d528
Dare Goodness <daregoodness@gmail.com>
2021-07-24 17:07:10 +0100

    twitter widgets

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import Widgets from './Components/Widgets/Widgets';
 function App() {
   return (
     <div className='app'>
-      {/* Sidebar */}
       <Sidebar />
-      {/* Feed */}
       <Feed />
-      {/* Widgets */}
       <Widgets />
     </div>
   );
--- src/Components/Widgets/Widgets.js
+++ src/Components/Widgets/Widgets.js
@@ -1,10 +1,34 @@
+import { Search } from '@material-ui/icons';
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from 'react-twitter-embed';
 import React from 'react';
 import './Widgets.css';
 
 const Widgets = () => {
   return (
     <div className='widgets'>
-      <h2>Widgets</h2>
+      <div className='widgetsInput'>
+        <Search className='widgetsSearchIcon' />
+        <input type='text' placeholder='Search Twitter' />
+      </div>
+
+      {/* Widget widgets container */}
+      <div className='widgetsWidgetContainer'>
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={'23451'} />
+
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='saurabhnemade'
+          options={{ height: 400 }}
+        />
+
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='_DareGoodness'
+          options={{ height: 400 }}
+        />
+      </div>
     </div>
   );
 };
